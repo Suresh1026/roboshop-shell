9d8775b6507124d02945737f133d635713e747ab
Suresh Dayyala <suresh.xlmac@gmail.com>
2024-01-30 11:57:37 +0530

    roboshop project details

---
+++ catalogue.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MONGODB_Host=mongodb.sureshdayyala.online
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R Failed $N"
+        exit 1
+    else
+        echo -e "$2 ...$G Success $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R error: plese run this script with root user $N"
+    exit 1
+else
+    echo "you are root user"
+fi
+
+dnf module disable nodejs -y &>> $LOGFILE
+
+VALIDATE $? "Disabling old nodejs" 
+
+dnf module enable nodejs:18 -y &>> $LOGFILE
+
+VALIDATE $? "enabling new nodejs" 
+
+dnf install nodejs -y
+
+VALIDATE $? "Installing nodejs" 
+
+useradd roboshop &>> $LOGFILE
+
+VALIDATE $? "adding user"
+
+mkdir /app &>> $LOGFILE
+
+VALIDATE $? "creating app directory" 
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
+
+VALIDATE $? "Downloading catalogue application" 
+
+unzip /tmp/catalogue.zip &>> $LOGFILE
+
+VALIDATE $? "Unzipping catalogue" 
+
+npm install &>> $LOGFILE
+
+VALIDATE $? "npm installing" 
+
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
+
+VALIDATE $? "Coping catalogue service file" 
+
+systemctl daemon-reload &>> $LOGFILE
+
+VALIDATE $? "catalogue daemon reloaded" 
+
+systemctl enable catalogue &>> $LOGFILE
+
+VALIDATE $? "enabled catalogue" 
+
+systemctl start catalogue &>> $LOGFILE
+
+VALIDATE $? "started catalogue" 
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "copying catalogue repo" 
+
+dnf install mongodb-org-shell -y &>> $LOGFILE
+
+VALIDATE $? "installing MongoDB client" 
+
+mongo --host $MONGODB_Host </app/schema/catalogue.js &>> $LOGFILE
+
+VALIDATE $? "Loading catalogue data into MongoDB" 
+
+
+
+
