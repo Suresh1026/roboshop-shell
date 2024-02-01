4355e7facfac485560f81ba3bc33e1ffedaf2406
Suresh Dayyala <suresh.xlmac@gmail.com>
2024-02-01 11:11:11 +0530

    roboshop project detils

---
+++ rbbitmq.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MONGDB_HOST=mongodb.sureshdayyala.online
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R Failed $N"
+        exit 1
+    else
+        echo -e "$2 ...$G Success $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R error: plese run this script with root user $N"
+    exit 1
+else
+    echo "you are root user"
+fi
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>> $LOGFILE
+
+VALIDATE $? "Downloading erlang script"
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>> $LOGFILE
+
+VALIDATE $? "Downloading rabbitmq script"
+
+dnf install rabbitmq-server -y &>> $LOGFILE
+
+VALIDATE $? "Instlling rabbitmq server"
+
+systemctl enable rabbitmq-server &>> $LOGFILE
+
+VALIDATE $? "Enabiling rabbitmq server"
+
+systemctl start rabbitmq-server &>> $LOGFILE
+
+VALIDATE $? "starting rabbitmq script"
+
+rabbitmqctl add_user roboshop roboshop123 &>> $LOGFILE
+
+VALIDATE $? "adding user"
+
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOGFILE
+
+VALIDATE $? "setting permision"
