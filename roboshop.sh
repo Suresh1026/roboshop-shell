e39abafdc0ca06f268ce6aa385cca2019b97b0fc
Suresh Dayyala <suresh.xlmac@gmail.com>
2024-02-08 19:37:28 +0530

    instances creation

--- roboshop.sh
+++ roboshop.sh
@@ -19,23 +19,23 @@ do
     echo "$i: $IP_ADDRESS"
 
     #create Route53 records, make sure you delete existing
-    aws route53 change-resource-record-sets \
-  --hosted-zone-id $ZONE_ID \
-  --change-batch '
-  {
-    "Comment": "Creating a record set for cognito endpoint"
-    ,"Changes": [{
-      "Action"              : "CREATE"
-      ,"ResourceRecordSet"  : {
-        "Name"              : "'$$i'.'$DOMAIN_NAME'"
-        ,"Type"             : "A"
-        ,"TTL"              : 1
-        ,"ResourceRecords"  : [{
-            "Value"         : "'$IP_ADDRESS'"
+        aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
         }]
-      }
-    }]
-  }
-  '
+    }
+    '
 done
 
