d59df6440396ae4c22f8c5ffd6e14c27d0ca06eb
Suresh Dayyala <suresh.xlmac@gmail.com>
2024-02-08 14:02:29 +0530

    instances creation

--- roboshop.sh
+++ roboshop.sh
@@ -13,9 +13,8 @@ do
     else
         INSTANCES_TYPE="t2.micro"
     fi
-
-
-    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCES_TYPE --security-group-ids sg-03bceb879f1cc352a --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$i}]"
+    
+    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCES_TYPE --security-group-ids sg-03bceb879f1cc352a --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
     
 done
 
