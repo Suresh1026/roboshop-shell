9c3d772d522a1c56dfb878b110b484e8dcf366ca
Suresh Dayyala <suresh.xlmac@gmail.com>
2024-02-08 12:49:02 +0530

    instances creation

---
+++ roboshop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+AMI=ami-0f3c7d07486cad139
+SG_ID=sg-03bceb879f1cc352a
+INSTANCES=("mongodb" "rabbitmq" "mysql" "Shipping" "user" "cart" "redis" "payment" "catalogue" "dispatch" "web")
+
+for i in "${INSTANCES[@]}"
+do
+    echo "instance is: $i"
+    if  [ $i = "mongodb" ] || [ $i = "mysql" ] || [ $i = "shipping" ]
+    then
+        INSTANCES_TYPE="t3.small"
+    else
+        INSTANCES_TYPE="t2.micro"
+    fi
+
+
+    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCES_TYPE 
+    --security-group-ids sg-03bceb879f1cc352a 
+    
+done
+
